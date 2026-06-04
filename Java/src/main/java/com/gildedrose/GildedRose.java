068ee6d4d691af742a2a7756574d6d9a4c78b7ee
Giovanni Massaro <github@demasso.be>
2026-06-04 19:22:37 +0200

    Got rid of inverted logiv part 1. Seems typical of business requirement that grew over the years maybe.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -14,30 +14,30 @@ class GildedRose {
 	public void updateQuality() {
 		// this loop is so much better.
 		for (Item item : items) {
-			// exclude _sulfur right away as nothing ever changes to producte
+			// exclude _sulfur right away as nothing ever changes to product
 			if (item.name.equals(_sulfur)) continue;
 
 			// 1. decrease sellIn for the product:
 			item.sellIn -- ;
 			
 			// 2. handle quality
-			if (!item.name.equals(_agedBr) && !item.name.equals(_backSt)) {
-				if (item.quality > 0) {
-					if (!item.name.equals(_sulfur)) item.quality -- ;
-				}
-			} else {
+			if (item.name.equals(_agedBr) || item.name.equals(_backSt)) {
 				if (item.quality < 50) {
+					// increase quality for _agedBr & _backSt
 					item.quality ++ ;
 
+					// _backSt quality improves even more nearing sellIn end
 					if (item.name.equals(_backSt)) {
 						if (item.sellIn < 10 && item.quality < 50) item.quality ++ ;
-
 						if (item.sellIn < 5 && item.quality < 50) item.quality ++ ;
 					}
 				}
+
+			} else if (item.quality > 0) {
+				item.quality -- ;
 			}
 
-			// 3. in case of negative sellIn:
+			// 3. TODO in case of negative sellIn:
 			if (item.sellIn < 0) {
 				if (!item.name.equals(_agedBr)) {
 					if (!item.name.equals(_backSt)) {
