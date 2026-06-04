fc54919f6c896466fda8660a86af3b88e0c6a68b
Giovanni Massaro <github@demasso.be>
2026-06-04 18:55:33 +0200

    observations

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -8,7 +8,7 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        // this loop is so oldskool
+        // this loop is so oldskool.
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
