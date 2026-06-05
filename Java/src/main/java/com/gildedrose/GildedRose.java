78b5c02a33bb559250d871933035ee96b0259b4f
Giovanni Massaro <github@demasso.be>
2026-06-05 14:15:12 +0200

    setQuality() method implemented. Not yet grouped by item.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,9 +13,9 @@ class GildedRose {
 	}
 
 	public void updateQuality() {
-		// Item item : items loop, maybe there's a better way?
+		// Item item : items loop, todo: maybe there's a better way?
 		for (Item item : items) {
-			// Refactor by implementing switch with 5 cases (1 normal, 4 exceptions), use subMethods()
+			// todo: Refactor by implementing switch with 5 cases (1 normal, 4 exceptions), use subMethods()
 
 			// Rule 0 : exclude _sulfur right away as nothing ever changes to product
 			if (item.name.equals(_sulfur)) continue;
@@ -26,39 +26,36 @@ class GildedRose {
 			// 2. handle quality
 			// Rule 2.1 : _agedBr && _backSt IMPROVE in quality
 			if (item.name.equals(_agedBr) || item.name.equals(_backSt)) {
-				// Rule 2.1.1 : quality can never be >50
-				if (item.quality < 50) {
-					// Rule 2.1.2 : _agedBr & _backSt increase in quality
-					item.quality ++ ; // >10 : +1
+				// Rule 2.1.2 : _agedBr & _backSt increase in quality
+				setQuality(item, 1); // >10 : +1
 
-					// Rule 2.1.3 : _backSt quality improves even more nearing sellIn end
-					if (item.name.equals(_backSt)) {
-						if (item.sellIn < 10 && item.quality < 50) item.quality ++ ; // <10 : +2
-						if (item.sellIn < 5 && item.quality < 50) item.quality ++ ; // < 5 : +3
-					}
+				// Rule 2.1.3 : _backSt quality improves even more nearing sellIn end
+				if (item.name.equals(_backSt)) {
+					if (item.sellIn < 10) setQuality(item, 1); // <10 : +2
+					if (item.sellIn <  5) setQuality(item, 1); // < 5 : +3
 				}
 
 			} else if (item.quality > 0) {
 				// Rule 2.2 : Lower Quality. Never negative!
-				item.quality -- ;
+				setQuality(item, -1);
 
 				// Rule 2.3 : _conjrd degrades twice as fast
-				if (item.name.equals(_conjrd)) item.quality -- ;
+				if (item.name.equals(_conjrd)) setQuality(item, -1);
 			}
 
 			// Rule 3 : in case of negative sellIn:
 			if (item.sellIn < 0) {
 				if (item.name.equals(_agedBr)) {
 					// 3.1. increase _agedBr quality when sellIn has passed
-					if (item.quality < 50) item.quality ++ ;
+					setQuality(item, 1);
 
 				} else if (item.name.equals(_backSt)) {
 					// 3.2. _backSt degrade immediately to 0
-					item.quality = 0 ;
+					setQuality(item, 0);
 
 				} else {
 					// 3.3. Quality degrades twice as fast, EXCEPT for above two items.
-					if (item.quality > 0) item.quality -- ;
+					setQuality(item, -1);
 				}
 			}
 		}
