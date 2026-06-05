36ac28b752cdbf2f187714679607fff06255bff9
Giovanni Massaro <github@demasso.be>
2026-06-05 14:46:49 +0200

    Use switch/case. More readable? Not sure. More performant? Maybe, still have ifs.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -15,31 +15,48 @@ class GildedRose {
 	public void updateQuality() {
 		// Item item : items loop, todo: maybe there's a better way?
 		for (Item item : items) {
-			// todo: Refactor by implementing switch with 5 cases (1 normal, 4 exceptions), use subMethods()
+			switch(item.name) {
+				case "Sulfuras, Hand of Ragnaros" :
+					break ;  // Exclude _sulfur right away as nothing ever changes to product
 
-			if (item.name.equals(_sulfur)) continue; // Rule 0 : exclude _sulfur right away as nothing ever changes to product
 
-			item.sellIn -- ; // Lower sellIn. CAN be negative!
-			
-			// Handle by item
-			if (item.name.equals(_agedBr)) { // Aged Brie
-				setQuality(item, 1); // >10 : +1
-				if (item.sellIn < 0) setQuality(item, 1); // quality incrases faster on negative sellIn
 
-			} else if (item.name.equals(_backSt)) { // Backstage
-				if (item.sellIn < 0) {
-					setQuality(item, 0); // Quality drops to 0 once sellIn reached
+				case "Aged Brie" :
+					item.sellIn -- ;
 
-				} else{
 					setQuality(item, 1); // >10 : +1
-					if (item.sellIn < 10) setQuality(item, 1); // <10 : +2
-					if (item.sellIn <  5) setQuality(item, 1); // < 5 : +3
-				}
+					if (item.sellIn < 0) setQuality(item, 1); // quality incrases faster on negative sellIn
 
-			} else if (item.quality > 0) { // normal items
-				setQuality(item, -1); // normal item's quality degrades
-				if (item.sellIn < 0) setQuality(item, -1); // degrades even more once sellIn passed
-				if (item.name.equals(_conjrd)) setQuality(item, -1); // Conjured rule
+					break ;
+
+
+				case "Backstage passes to a TAFKAL80ETC concert" :
+					item.sellIn -- ;
+
+					if (item.sellIn < 0) {
+						setQuality(item, 0); // Quality drops to 0 once sellIn reached
+
+					} else {
+						setQuality(item, 1); // rule 4 & 6
+						if (item.sellIn < 10) setQuality(item, 1); // <10 : +2
+						if (item.sellIn <  5) setQuality(item, 1); // < 5 : +3
+					}
+					break ;
+					
+				/* Conjured case grouped with normal
+				case "Conjured Mana Cake" :
+					item.sellIn -- ;
+					break ;*/
+					
+				default :
+					item.sellIn -- ;
+
+					if (item.quality > 0) { // normal items
+						setQuality(item, -1); // normal item's quality degrades
+						if (item.sellIn < 0) setQuality(item, -1); // degrades even more once sellIn passed
+						if (item.name.equals(_conjrd)) setQuality(item, -1); // Conjured rule
+					}
+					break ;
 			}
 		}
 	}
