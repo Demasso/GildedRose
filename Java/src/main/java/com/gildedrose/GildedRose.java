3d329f0c30ed2bcc035cbd8929281e72efa20036
Giovanni Massaro <github@demasso.be>
2026-06-05 17:39:20 +0200

    Added sitItem() method in Item class.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,10 +1,6 @@
 package com.gildedrose;
 
 class GildedRose {
-	private static String _sulfur = "Sulfuras, Hand of Ragnaros";   // never touched
-	private static String _backSt = "Backstage passes to a TAFKAL80ETC concert";
-	private static String _agedBr = "Aged Brie";
-	private static String _conjrd = "Conjured Mana Cake";
 
 	Item[] items;
 
@@ -15,42 +11,25 @@ class GildedRose {
 	public void updateQuality() {
 		// Item item : items loop, todo: maybe there's a better way?
 		for (Item item : items) {
-			// todo: Refactor by implementing switch with 5 cases (1 normal, 4 exceptions), use subMethods()
-
-			if (item.name.equals(_sulfur)) continue; // Rule 0 : exclude _sulfur right away as nothing ever changes to product
+			// Rule 0 : exclude "Sulfuras, Hand of Ragnaros" right away as nothing ever changes to product
+			if (item.name.equals("Sulfuras, Hand of Ragnaros")) continue;
 
 			item.sellIn -- ; // Lower sellIn. CAN be negative!
-			
-			// Handle by item
-			if (item.name.equals(_agedBr)) { // Aged Brie
-				setQuality(item, 1); // >10 : +1
-				if (item.sellIn < 0) setQuality(item, 1); // quality incrases faster on negative sellIn
-
-			} else if (item.name.equals(_backSt)) { // Backstage
-				if (item.sellIn < 0) {
-					setQuality(item, 0); // Quality drops to 0 once sellIn reached
-
-				} else{
-					setQuality(item, 1); // >10 : +1
-					if (item.sellIn < 10) setQuality(item, 1); // <10 : +2
-					if (item.sellIn <  5) setQuality(item, 1); // < 5 : +3
-				}
-
-			} else if (item.quality > 0) { // normal items
-				setQuality(item, -1); // normal item's quality degrades
-				if (item.sellIn < 0) setQuality(item, -1); // degrades even more once sellIn passed
-				if (item.name.equals(_conjrd)) setQuality(item, -1); // Conjured rule
-			}
-		}
-	}
-
-	private void setQuality(Item item, int qlty){
-		if (qlty == 0) {
-			item.quality = 0 ;
-
-		} else if (item.quality < 50) {
-			item.quality += qlty ;
-			item.quality = item.quality < 0 ? 0 : item.quality ; // Quality cannot be negative!
+			setItem();
 		}
 	}
 }
+
+/*
+	RUN INCLUDED TEST:
+
+	in root dir:
+	./gradlew -q text --args 30
+
+	or
+	
+	in Java dir:
+	python3 texttest_rig.py -a 30
+
+	Diff compare output with texttests/ThirtyDays/stdout.gr
+*/
--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -14,6 +14,39 @@ public class Item {
         this.quality = quality;
     }
 
+    void setItem(){
+        // Handle by item
+        if (name.equals("Aged Brie")) { // Aged Brie
+            setQuality(1); // >10 : +1
+            if (sellIn < 0) setQuality(1); // quality incrases faster on negative sellIn
+
+        } else if (name.equals("Backstage passes to a TAFKAL80ETC concert")) { // Backstage
+            if (sellIn < 0) {
+                setQuality(0); // Quality drops to 0 once sellIn reached
+
+            } else{
+                setQuality(1); // >10 : +1
+                if (sellIn < 10) setQuality(1); // <10 : +2
+                if (sellIn <  5) setQuality(1); // < 5 : +3
+            }
+
+        } else if (quality > 0) { // normal items
+            setQuality(-1); // normal item's quality degrades
+            if (sellIn < 0) setQuality(-1); // degrades even more once sellIn passed
+            if (name.equals("Conjured Mana Cake")) setQuality(-1); // Conjured rule
+        }
+    }
+
+    private void setQuality(int qlty){
+        if (qlty == 0) {
+            quality = 0 ;
+
+        } else if (quality < 50) {
+            quality += qlty ;
+            quality = quality < 0 ? 0 : quality ; // Quality cannot be negative!
+        }
+    }
+
    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
